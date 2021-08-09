8abde8e54ef80ce3beac8af1d9f24bdb68c89cb8
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2021-08-09 12:50:04 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+diagnosis = Category.create(name: 'diagnosis')
+insurance = Category.create(name: 'insurance')
+treatment = Category.create(name: 'treatent')
+
+Action.create([
+    {name: 'ASD Diagnosis', description: 'get diagnosis from appropraite clinician', category: diagnosis},
+    {name: 'Get insurance', description: 'make sure you have insurance', category: insurance},
+    {name: 'Clinic takes insurance', description: 'confirm the clinic takes your insurance', category: insurance},
+    {name: 'Sessions',description: 'schedule consistent sessions', category: treatment},
+])
